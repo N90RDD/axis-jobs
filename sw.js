82b8bb017f9772ca4f909ea378f64944a07d8896
N90RDD <robbdawson1990@gmail.com>
2026-08-22 22:18:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE="axis-jobs-photo-fix-v3";
+const ASSETS=[
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "config.js",
+  "manifest.webmanifest",
+  "axis-logo.png"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
+  );
+});
