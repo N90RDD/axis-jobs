5c28a75639796a896d3e32e3944275ef5df62d01
N90RDD <robbdawson1990@gmail.com>
2026-08-22 22:39:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="axis-jobs-proper-ui-v5";
+const ASSETS=["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","axis-logo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
